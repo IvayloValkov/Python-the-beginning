6c83cb9e4508016d137ef63157dfbab39b1a8e2b
IvayloValkov <75259581+IvayloValkov@users.noreply.github.com>
2021-01-17 10:30:11 +0200

    Delete project_creation.py

--- project_creation.py
+++
@@ -1,4 +0,0 @@
-architect_name = input()
-number_project = int(input())
-number_hours_ = number_project * 3
-print(f'The architect {architect_name} will need {number_hours_} hours to complete {number_project} project/s.')
