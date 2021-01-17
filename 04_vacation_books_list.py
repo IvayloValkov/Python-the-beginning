77adc7335bb8602f720542f9753063ef446ef0b4
IvayloValkov <75259581+IvayloValkov@users.noreply.github.com>
2021-01-17 10:28:39 +0200

    Delete 04_vacation_books_list.py

--- 04_vacation_books_list.py
+++
@@ -1,9 +0,0 @@
-pages_count = int(input())
-pages_per_hour = int(input())
-days_count = int(input())
-
-total_time = pages_count / pages_per_hour
-total_hours = total_time / days_count
-print(total_hours)
-
-
