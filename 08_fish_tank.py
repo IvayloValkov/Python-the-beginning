8c44e96c8919146b5a24ce8f1d6141f2801f917b
IvayloValkov <75259581+IvayloValkov@users.noreply.github.com>
2021-01-17 10:29:17 +0200

    Delete 08_fish_tank.py

--- 08_fish_tank.py
+++
@@ -1,9 +0,0 @@
-lenght = int(input())
-width = int(input())
-height = int(input())
-percent = float(input())
-
-volume = height * width * lenght
-liters = volume / 1000
-result = liters * (1 - percent / 100)
-print(result)
