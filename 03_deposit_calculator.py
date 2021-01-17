8d76ea691f509c83c28ddd626600d6e19bf973f5
IvayloValkov <75259581+IvayloValkov@users.noreply.github.com>
2021-01-17 10:28:31 +0200

    Delete 03_deposit_calculator.py

--- 03_deposit_calculator.py
+++
@@ -1,8 +0,0 @@
-money = float(input())
-months = int(input())
-interest_rate = float(input())
-
-interest = money * interest_rate / 100
-interest_per_month = interest / 12
-total_money = money + (months * interest_per_month)
-print(total_money)
