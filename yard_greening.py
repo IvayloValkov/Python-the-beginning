28d4f0f11bc82989cd0915e4b72f974ef7e871c8
IvayloValkov <75259581+IvayloValkov@users.noreply.github.com>
2021-01-17 10:30:24 +0200

    Delete yard_greening.py

--- yard_greening.py
+++
@@ -1,8 +0,0 @@
-square_meters = float(input())
-square_meter_price = 7.61
-
-price = square_meters * square_meter_price
-discount = (square_meters * square_meter_price * 0.18)
-final_price = price - discount
-print(f'The final price is {final_price} lv')
-print((f'The Discount is {discount} lv'))
